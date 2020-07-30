6fdb278f4928d2cdba9ec8ce1a8e4f669f23fc7f
felipefariasdasilva <feliipefarias@outlook.com>
2020-07-30 20:12:21 -0300

    fixed package.json files

--- src/components/Categoria/index.js
+++ src/components/Categoria/index.js
@@ -26,7 +26,10 @@ export default function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/categorias'
+    : '';
+
     fetch(URL)
       .then(async (serverResponse) => {
         const response = await serverResponse.json();
